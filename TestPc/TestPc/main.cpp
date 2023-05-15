63111f2ee6b1f763b90d2d686fe220220877e52d
Bagg3 <andreasbaggenielsen@gmail.com>
2023-05-15 14:29:39 +0200

    Just test

--- TestPc/TestPc/main.cpp
+++ TestPc/TestPc/main.cpp
@@ -7,135 +7,135 @@
 
 #define DATA_LENGTH 255
 
-const char *portName = "\\\\.\\COM3";
+const char* portName = "\\\\.\\COM3";
 
 // Declare a global object
-SerialPort *arduino;
+SerialPort* arduino;
 std::string receivedData;
 
 int main()
 {
 
-    std::vector<std::string> data;
-    arduino = new SerialPort(portName);
-    arduino->isConnected();
-    std::cout << "Is connected: " << arduino->isConnected() << std::endl;
+	std::vector<std::string> data;
+	arduino = new SerialPort(portName);
+	arduino->isConnected();
+	std::cout << "Is connected: " << arduino->isConnected() << std::endl;
 
-    while (arduino->isConnected())
-    {
-        char receivedChar;
-        int hasRead = arduino->readSerialPort(&receivedChar, 1);
-        if (hasRead)
-        {
-            if (receivedChar != '\n')
-            {
-                receivedData += receivedChar;
-            }
-            else
-            {
-                // std::cout << receivedData << std::endl;
+	while (arduino->isConnected())
+	{
+		char receivedChar;
+		int hasRead = arduino->readSerialPort(&receivedChar, 1);
+		if (hasRead)
+		{
+			if (receivedChar != '\n')
+			{
+				receivedData += receivedChar;
+			}
+			else
+			{
+				// std::cout << receivedData << std::endl;
 
-                data.push_back(receivedData);
-                receivedData.clear();
-                if (data.size() == 2)
-                {
-                    arduino->closeSerial();
-                }
-            }
-        }
-    }
+				data.push_back(receivedData);
+				receivedData.clear();
+				if (data.size() == 2)
+				{
+					arduino->closeSerial();
+				}
+			}
+		}
+	}
 
-    for (int j = 0; j < data.size(); j++)
-    {
+	for (int j = 0; j < data.size(); j++)
+	{
 
-        data[j].push_back(' ');
-        int rum = 10;
+		data[j].push_back(' ');
+		int rum = 10;
 
-        // Function to convert exempel to int array, split on " "
-        std::string delimiter = " ";
-        size_t pos = 0;
-        std::string token;
-        int i = 0;
-        int arr[10];
-        while ((pos = data[j].find(delimiter)) != std::string::npos)
-        {
-            token = data[j].substr(0, pos);
-            // std::cout << token << std::endl;
-            arr[i] = std::stoi(token);
-            data[j].erase(0, pos + delimiter.length());
-            i++;
-        }
-        // std::cout << arr << std::endl;
+		// Function to convert exempel to int array, split on " "
+		std::string delimiter = " ";
+		size_t pos = 0;
+		std::string token;
+		int i = 0;
+		int arr[10];
+		while ((pos = data[j].find(delimiter)) != std::string::npos)
+		{
+			token = data[j].substr(0, pos);
+			// std::cout << token << std::endl;
+			arr[i] = std::stoi(token);
+			data[j].erase(0, pos + delimiter.length());
+			i++;
+		}
+		// std::cout << arr << std::endl;
 
-        // function to loop through array and find the highest value
-        int highest = 0;
-        for (int i = 0; i < rum; i++)
-        {
-            if (arr[i] > highest)
-            {
-                highest = arr[i];
-            }
-        }
+		// function to loop through array and find the highest value
+		int highest = 0;
+		for (int i = 0; i < rum; i++)
+		{
+			if (arr[i] > highest)
+			{
+				highest = arr[i];
+			}
+		}
 
-        std::cout << highest << std::endl;
-        int TOTAL[10];
-        TOTAL[j] = highest;
+		std::cout << highest << std::endl;
+		int TOTAL[10];
+		TOTAL[j] = highest;
 
-        for (int k = 0; k < rum; k++)
-        {
-            if (arr[k] == highest)
-            {
-                std::cout << "Person " << i << " "
-                          << "Rum " << k + 1 << " er det mest besogte rum" << std::endl;
-            }
-        }
-    }
+		for (int k = 0; k < rum; k++)
+		{
+			if (arr[k] == highest)
+			{
+				std::cout << "Person " << i << " "
+					<< "Rum " << k + 1 << " er det mest besogte rum" << std::endl;
+			}
+		}
+	}
 
-    /*
-    std::string Bruger1;
+	/*
+	std::string Bruger1;
 
-    for (size_t i = 0; i < data.size(); i++) {
-        std::cout << data[i] << std::endl;
-        char* dataPtr = &data[i][0];
+	for (size_t i = 0; i < data.size(); i++) {
+		std::cout << data[i] << std::endl;
+		char* dataPtr = &data[i][0];
 
-        char* ptr = strtok(data[i], ",");
+		char* ptr = strtok(data[i], ",");
 
-        while (dataPtr != ",") {
-            ;
-            Bruger1 = *dataPtr;
-            dataPtr++;
-        }
+		while (dataPtr != ",") {
+			;
+			Bruger1 = *dataPtr;
+			dataPtr++;
+		}
 
-    }
+	}
 
-    std::cout << Bruger1 << std::endl;
+	std::cout << Bruger1 << std::endl;
 
-    */
-    return 0;
+	*/
+	return 0;
 }
 
 // else std::cerr << "Error occured reading data" << "\n";
 // Sleep(50);
 
 /*
-    PCHandler PC("admin");
-    PC.clearScreen();
-    std::cout << "Welcome to the IPRS system" << std::endl;
-    PC.showMenu();
+	PCHandler PC("admin");
+	PC.clearScreen();
+	std::cout << "Welcome to the IPRS system" << std::endl;
+	PC.showMenu();
 
-    return 0;
+	return 0;
 
-    */
+	*/
 
-/*
-# include<windows.h>
-HANDLE console = GetStdHandle(STD_INPUT_HANDLE);
-DWORD consoleMode;
-GetConsoleMode(console, &consoleMode);
+	/*
+	# include<windows.h>
+	HANDLE console = GetStdHandle(STD_INPUT_HANDLE);
+	DWORD consoleMode;
+	GetConsoleMode(console, &consoleMode);
 
-//Remove echo from console
-SetConsoleMode(console, consoleMode & ~ENABLE_ECHO_INPUT);
+	//Remove echo from console
+	SetConsoleMode(console, consoleMode & ~ENABLE_ECHO_INPUT);
 
-// Set console to default
-SetConsoleMode(console, consoleMode);
-*/
+	// Set console to default
+	SetConsoleMode(console, consoleMode);
+	*/
